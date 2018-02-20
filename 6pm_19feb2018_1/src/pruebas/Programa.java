ae5ee8b3636aef22d359714ccd8d57580ce13ac3
onivia <ohnivia@gmail.com>
2018-02-19 20:24:11 -0500

    ejemplo.

--- 6pm_19feb2018_1/src/pruebas/Programa.java
+++ 6pm_19feb2018_1/src/pruebas/Programa.java
@@ -15,22 +15,16 @@ public class Programa {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int i = 15;
-        int j = 10;
+        int i = 0;
+        for (i = 0; i < 10; i++) {
+            //System.out.println(i);            
+        }
         
-        //&& short circuit
-        //&
-        
-        switch(i) {
-            case 10:
-            case 5:
-                System.out.println("i es igual a 5 o a 10");
-                break;
-            default:
-                System.out.println("i no es 5 ni 10");
-                break;
-        }       
-       
+        i = 0;
+        while(i<10) {
+            System.out.println(i);            
+        }
+        //System.out.println(i);
     }
 
     public static boolean evalua(int numero, int comparador) {
