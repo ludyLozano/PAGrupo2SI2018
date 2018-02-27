dcd992818b21fed160b9109cab84bd2bd0a02fc8
onivia <ohnivia@gmail.com>
2018-02-26 19:36:28 -0500

    ejercicio 10.

---
+++ 6pm_26feb2018_1/src/pruebas/Ejercicio10.java
@@ -0,0 +1,18 @@
+
+package pruebas;
+
+/**
+ *
+ * @author ONivia
+ */
+public class Ejercicio10 {
+    public static void main(String[] args) {
+        int numero = 925;
+        
+        System.out.println(numero % 10);
+
+        System.out.println((numero % 100) / 10);
+        
+        System.out.println((numero % 1000) / 100);
+    }    
+}
