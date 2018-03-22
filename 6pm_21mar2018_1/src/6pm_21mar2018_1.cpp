9bd29d5d986bfa5a3f0e547e2aae50bbe5aa90a6
onivia <ohnivia@gmail.com>
2018-03-21 19:05:21 -0500

    tratamiento de arreglos como apuntadores, se examinaron los espacios de memoria.

--- 6pm_21mar2018_1/src/6pm_21mar2018_1.cpp
+++ 6pm_21mar2018_1/src/6pm_21mar2018_1.cpp
@@ -9,34 +9,20 @@
 #include <iostream>
 using namespace std;
 
-//tratamiento de apuntadores x valor
-void f1(int *p1) {
-	int b = 20;
+void f1(int **p1) {
+	//cout<<*(p1 + 2)<<endl; //aritmetica de apuntadores
+	//cout<<p1[2]<<endl;
 
-	p1 = &b;
-
-	cout<<*p1<<endl; //2°: 20
-}
-
-//tratamiento de apuntadores x referencia
-void f2(int **p1) {
-	int b = 20;
-
-	*p1 = &b;
-
-	cout<<**p1<<endl; //2°: 20
+	cout<<&p1<<endl;
 }
 
 int main() {
-	int a = 10;
+	int nums[] = {10,20,30};
 	int *p = NULL;
 
-	p = &a;
+	p = nums;
 
-	cout<<*p<<endl; //1°: 10
-	//f1(p);
-	f2(&p);
-	cout<<*p<<endl; //3° 20
+	f1(&p);
 
 	return 0;
 }
